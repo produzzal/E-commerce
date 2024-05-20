79eda90506b5b6ba845b09343a03fcbee639c797
prouzzal <prouzzal297@gmail.com>
2024-05-20 20:03:27 +0600

    interface done

---
+++ src/modules/E-commerce/E-commerce.interface.ts
@@ -0,0 +1,19 @@
+type TVariant = {
+  type: string;
+  value: string;
+};
+
+type TInventory = {
+  quantity: number;
+  inStock: boolean;
+};
+
+type TProduct = {
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  tags: [string];
+  variants: [TVariant];
+  inventory: TInventory;
+};
